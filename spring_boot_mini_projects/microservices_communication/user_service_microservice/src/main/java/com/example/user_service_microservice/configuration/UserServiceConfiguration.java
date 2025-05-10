fc54349ea456b36c425ce09ac8472c3a6a9f3f1b
Pranjal Mehrotra <pranjalmehrotra213@gmail.com>
2025-05-10 10:16:54 +0530

    Adding more files

--- spring_boot_mini_projects/microservices_communication/user_service_microservice/src/main/java/com/example/user_service_microservice/configuration/UserServiceConfiguration.java
+++ spring_boot_mini_projects/microservices_communication/user_service_microservice/src/main/java/com/example/user_service_microservice/configuration/UserServiceConfiguration.java
@@ -1,6 +1,4 @@
 package com.example.user_service_microservice.configuration;
-
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
